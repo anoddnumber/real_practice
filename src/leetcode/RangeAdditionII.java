1e5b5285044f108e476e40fb3d16db1596517c9d
anoddnumber <suchanoddnumber@gmail.com>
2019-03-02 14:14:36 -0800

    RangeAdditionII

---
+++ src/leetcode/RangeAdditionII.java
@@ -0,0 +1,12 @@
+package leetcode;
+
+public class RangeAdditionII {
+
+    public int maxCount(int m, int n, int[][] ops) {
+        for (int i = 0; i < ops.length; i++) {
+            m = Math.min(m, ops[i][0]);
+            n = Math.min(n, ops[i][1]);
+        }
+        return m * n;
+    }
+}
