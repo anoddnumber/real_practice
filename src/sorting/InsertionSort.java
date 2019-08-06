16b0f856e2e579d9dc7dd05d4b2b334cfd3689e7
anoddnumber <suchanoddnumber@gmail.com>
2019-08-05 18:52:25 -0700

    InsertionSort

---
+++ src/sorting/InsertionSort.java
@@ -0,0 +1,24 @@
+package sorting;
+
+import java.util.Arrays;
+
+public class InsertionSort {
+
+    static void sort(int[] nums) {
+        for (int i = 1; i < nums.length; i++) {
+            int j = i;
+            while (j >= 1 && nums[j] < nums[j-1]) {
+                int tmp = nums[j];
+                nums[j] = nums[j-1];
+                nums[j-1] = tmp;
+                j--;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {4,2,7,3,8,5};
+        BubbleSort.sort(nums);
+        System.out.println(Arrays.toString(nums));
+    }
+}
